92cfcc913e121e8fa65982103e18f2cca58008ae
Rodri Valdivia <rodrivaldivia10@gmail.com>
2020-01-16 03:24:16 -0300

    intento de usar super-res cortadas en pedacitos

--- autoencoder_train.py
+++ autoencoder_train.py
@@ -10,31 +10,70 @@ from keras import backend as K
 from keras.datasets import mnist
 import numpy as np
 import matplotlib.pyplot as plt
-from PIL import Image
+from PIL import Image, ImageEnhance
 import matplotlib.image as mpimg
 
 np.random.seed(1337)
 
+
+def get_image(fileName):
+  im = Image.open(fileName)
+  image = Image.fromarray(im / np.amax(im) * 255)
+  # im.mode = 'I'
+  im = im.point(lambda i:i*(1./256)).convert('L')
+  size = 32, 32
+  image.thumbnail(size, Image.ANTIALIAS)
+  # enhancer = ImageEnhance.Brightness(im)
+  # enhanced_im = enhancer.enhance(2.0)
+  # im.save("enhanced.sample1.png")
+  # exit()
+  # return enhanced_im
+  return image
+
+def load_train_img(fileName):
+  image = Image.open(fileName)
+  # image = Image.fromarray(im / np.amax(im) * 255)
+  arr = np.asarray(image)
+  split = arr.shape[1]/32 #256
+  arr = np.split(arr, split)
+  arr = np.array([np.split(x, split, 1) for x in arr])
+  return arr
+
 # MNIST dataset
-(x_train, _), (x_test, _) = mnist.load_data()
-print('MNIST Shape: ', x_train.shape)
+# (x_train, _), (x_test, _) = mnist.load_data()
+# print('MNIST Shape: ', x_train.shape)
+
+# Microscope dataset
+# train = []
+# for i in range(1,120):
+#   fileName = 'canal_1/s_C001T'
+#   fileName += '%0*d' % (3, i)
+#   fileName += '.tif'
+#   # train.append(mpimg.imread(fileName))
+#   # exit()
+#   im = get_image(fileName)
+#   train.append(np.array(im))
+# x_train = np.asarray(train)
+# print('TPP Shape: ', x_train.shape)
 
 # Microscope dataset
 train = []
-for i in range(1,120):
-  fileName = 'canal_1/s_C001T'
-  fileName += '%0*d' % (3, i)
-  fileName += '.tif'
-  train.append(mpimg.imread(fileName))
+for i in range(1,16):
+  arr = load_train_img('train/train%d.tif' % i)
+  for row in arr:
+    for im in row:
+      # print(im.shape)
+      train.append(im)
 x_train = np.asarray(train)
-print('TPP Shape: ', x_train.shape)
 
-test = []
-for i in range(120,151):
+
+test = [ x_train[1], x_train[2], ]
+for i in range(1,20): #151):
   fileName = 'canal_1/s_C001T'
   fileName += '%0*d' % (3, i)
   fileName += '.tif'
-  test.append(mpimg.imread(fileName))
+  im = get_image(fileName)
+  test.append(np.array(im))
 x_test = np.asarray(test)
 
 
@@ -51,8 +90,8 @@ x_test = x_test.astype('float32') / 255
 # centered at 0.5 and std=0.5
 noise = np.random.normal(loc=0.5, scale=0.5, size=x_train.shape)
 x_train_noisy = x_train + noise
-noise = np.random.normal(loc=0.5, scale=0.5, size=x_test.shape)
-x_test_noisy = x_test + noise
+# noise = np.random.normal(loc=0.5, scale=0.5, size=x_test.shape)
+x_test_noisy = x_test
 
 x_train_noisy = np.clip(x_train_noisy, 0., 1.)
 x_test_noisy = np.clip(x_test_noisy, 0., 1.)
@@ -61,6 +100,7 @@ x_test_noisy = np.clip(x_test_noisy, 0., 1.)
 input_shape = (image_size, image_size, 1)
 batch_size = 128
 kernel_size = 3
+# Vamos a tener que agrandar esto seguro
 latent_dim = 16
 # Encoder/Decoder number of CNN layers and filters per layer
 layer_filters = [32, 64]
